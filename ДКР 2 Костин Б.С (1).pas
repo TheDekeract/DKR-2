f60d04517789e989655bdddc56d65ebecf3efd8a
Борис Сергеевич <117807416+TheDekeract@users.noreply.github.com>
2022-11-29 15:59:52 +0300

    Add files via upload

---
+++ ДКР 2 Костин Б.С (1).pas
@@ -0,0 +1,14 @@
+﻿begin
+  var a: array[1..100] of integer;
+  var min, n: integer;
+  print('Размер массива от 1 до 100, введите n = ');
+  readln(n);   
+  if (n < 1) and (n > 100) then exit;
+  print('Введите массив А:');
+  for var i := 1 to n do read(a[i]);
+  writeln;
+  min := 100;
+  for var i := 1 to n do
+    if abs(a[i]) < abs(min) then min := a[i];
+  print('Минимум по модулю = ', abs(min));
+end.
---
+++ ДКР 2 Костин Б.С (2+3).pas
@@ -0,0 +1,32 @@
+﻿begin
+  var  j, maxkol, kol: integer;
+  var s, w, slow, max: string;
+  j := 0;
+  kol := 0;
+  maxkol := 0;
+  w := '';
+  print('Предложение, словосочетание, надпись: ');
+  readln(s);
+  s := s + ' ';
+  for var i := 1 to length(s) do if s[i] <> ' ' then w := w + s[i] else j := j + 1;
+  w := '';
+  for var i := 1 to length(s) do
+    if s[i] <> ' ' then w := w + s[i] else
+    begin
+      if slow <> w then kol := 0;
+      slow := w;
+      for var k := 1 to j do
+        if s[i] <> ' ' then w := w + s[i] else
+        begin
+          if slow = w then kol := kol + 1;
+          if maxkol < kol then
+          begin
+            maxkol := kol;
+            max := slow;
+          end;
+          w := '';
+        end;
+      w := '';
+    end;
+  print(max);
+end.
